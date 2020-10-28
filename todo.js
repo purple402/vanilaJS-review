4388f6fb421eab49d4f35e0298b5cefcb635b883
purple402 <purple402@naver.com>
2020-10-28 19:49:55 +0900

    Make Todo

---
+++ todo.js
@@ -0,0 +1,64 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+let toDos = [];
+
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return JSON.stringify(toDo.id) !== li.id;
+    })
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const textli = document.createTextNode(text);
+    const delBtn = document.createElement("button");
+    const newId = Date.now() + toDos.length;
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
+    li.appendChild(delBtn);
+    li.appendChild(textli);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    }
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmitToDo(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = ""; //submit 후 input창 초기화
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        })
+    }
+}
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmitToDo)
+}
+init();
