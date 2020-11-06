d413f11f65f1c06ebb6ea51ae5704d7ded2988ce
purple402 <purple402@naver.com>
2020-11-06 23:34:00 +0900

    Make weather icon

--- weather.js
+++ weather.js
@@ -1,7 +1,41 @@
-const weather = document.querySelector(".js-weather");
+const weather = document.querySelector(".js-weather"),
+    weatherIcon = document.querySelector(".js-weather_icon");
 
 const API_KEY = config.MY_KEY;
 const COORDS = 'coords';
+
+function getIcon(iconID){
+    // icon source = https://erikflowers.github.io/weather-icons/
+    const currentIcon = document.createElement("icon");
+    const iconList = {
+        '01d' : "wi-day-sunny",
+        '02d' : "wi-day-cloudy",
+        '03d' : "wi-cloud",
+        '04d' : "wi-cloudy",
+        '09d' : "wi-rain",
+        '10d' : "wi-day-rain",
+        '11d' : "wi-thunderstorm",
+        '13d' : "wi-snow",
+        '50d' : "wi-fog",
+        '01n' : "wi-night-clear",
+        '02n' : "wi-night-alt-cloudy",
+        '03n' : "wi-cloud",
+        '04n' : "wi-cloudy",
+        '09n' : "wi-rain",
+        '10n' : "wi-rain",
+        '11n' : "wi-thunderstorm",
+        '13n' : "wi-night-alt-snow",
+        '50n' : "wi-night-fog"
+    };
+    /*
+    key 값으로 숫자가 먼저 오면 iconList.iconID시 오류남
+    참고 : JavaScript Object[‘key’] vs Object.key 차이
+    https://medium.com/sjk5766/javascript-object-key-vs-object-key-%EC%B0%A8%EC%9D%B4-3c21eb49b763
+    */
+    const iconClass = iconList[iconID];
+    currentIcon.classList.add('wi', iconClass);
+    weatherIcon.appendChild(currentIcon);
+}
 
 function getWeather(lat, lon){
     //fecth는 서버에 요청을 보내고 자료를 받아옴
@@ -12,13 +46,15 @@ function getWeather(lat, lon){
         const place = json.name;
         const tempSpan = document.createElement('span');
         const placeSpan = document.createElement('span');
-        console.log(json);
         tempSpan.classList.add("temp");
-        tempSpan.innerText = `${temp}°C`;
+        tempSpan.innerText = `${temp}°`;
         placeSpan.classList.add("place");
         placeSpan.innerText = ` @ ${place}`;
         weather.appendChild(tempSpan);
         weather.appendChild(placeSpan);
+
+        const icon = json.weather[0].icon;
+        getIcon(icon);
     })
 }
 
