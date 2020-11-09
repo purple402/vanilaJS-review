36a586f8398f9d9e77edd7039853948f02999773
purple402 <purple402@naver.com>
2020-11-09 17:04:53 +0900

    delete seconds

--- clock.js
+++ clock.js
@@ -12,9 +12,11 @@ function getTime(){
     if (hours == 0) {hours = 12};
     
     ampm.innerText = `${ap}`;
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes}` : minutes}`;
     
 }
+
+// : ${seconds < 10 ? `0${seconds}` : seconds}
 
 function init(){
     getTime();
